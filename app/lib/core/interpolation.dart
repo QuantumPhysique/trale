9cba4f82c70fff3d3ac2d32374f4102966142965
braniii <daniel.nagel@protonmail.com>
2021-10-18 10:16:42 +0200

    Add class for interpolation and an API for accessing Hive elements.

---
+++ app/lib/core/interpolation.dart
@@ -0,0 +1,117 @@
+import 'dart:math';
+
+import 'package:trale/core/measurement.dart';
+
+
+/// return gaussian with std of 3 days in x [ms]
+double gaussian(
+    double x, {double sigma=72 * 3600 * 1000, double mu=0}
+) => 1 / (sigma * sqrt(2 * pi)) * exp(-1 / 2 * pow(x - mu, 2) / pow(sigma, 2));
+
+
+/// x date in milliseconds since epoch, measurements sorted by date
+Measurement gaussianInterpol(int x, List<Measurement> measurements){
+  double weightSum = 0;
+  double meanSum = 0;
+  for (final Measurement m in measurements) {
+    final double weight = gaussian(
+      x.toDouble(),
+      mu: m.dateInMs.toDouble(),
+      sigma: (m.isMeasured ? 4 : 2) * 24 * 3600 * 1000,
+    ) * (m.isMeasured ? 10 : 1);
+    weightSum += weight;
+    meanSum += m.weight * weight;
+  }
+  return Measurement(
+    weight: meanSum / weightSum,
+    date: DateTime.fromMillisecondsSinceEpoch(x),
+    isMeasured: false,
+  );
+}
+
+/// x date in milliseconds since epoch, measurements sorted by date
+Measurement linearInterpol(int x, List<Measurement> measurements){
+  final int index = measurements.lastIndexWhere(
+    (Measurement m) => m.dateInMs <= x
+  );
+  int indexBefore, indexAfter;
+  if (index == -1) {
+    indexBefore = 0;
+    indexAfter = 1;
+  } else if (index == measurements.length - 1) {
+    indexBefore = measurements.length - 2;
+    indexAfter = measurements.length - 1;
+  } else {
+    indexBefore = index;
+    indexAfter = index + 1;
+  }
+  final Measurement mBefore = measurements[indexBefore];
+  final Measurement mAfter = measurements[indexAfter];
+
+  final double derivative = (
+    mAfter.weight - mBefore.weight
+  ) / (
+    mAfter.dateInMs - mBefore.dateInMs
+  );
+  final double offset = mAfter.weight - derivative * mAfter.dateInMs;
+  return Measurement(
+    weight: derivative * x + offset,
+    date: DateTime.fromMillisecondsSinceEpoch(x),
+    isMeasured: false,
+  );
+}
+
+
+/// Enum with all available interpolation functions
+enum InterpolFunc {
+  /// gaussian
+  gaussian,
+  /// stones
+  linear,
+}
+
+/// extend units
+extension InterpolFuncExtension on InterpolFunc {
+  /// get the scaling factor to kg
+  Function(int, List<Measurement>) get function =>
+    <InterpolFunc, Function(int, List<Measurement>)>{
+      InterpolFunc.gaussian: gaussianInterpol,
+      InterpolFunc.linear: linearInterpol,
+    }[this]!;
+  /// get string expression
+  String get name => toString().split('.').last;
+}
+
+/// class for interpolating measurement values
+class Interpolation {
+  /// constructor
+  Interpolation({
+    required this.measures,
+    /// Step width [days]
+    double extrapolationStepWidth=1,
+    /// Extrapolation range [days]
+    double extrapolationRange=7,
+  }) {
+    this.extrapolationRange = (extrapolationRange * 24*3600*1000).toInt();
+    this.extrapolationStepWidth = (
+      extrapolationStepWidth * 24*3600*1000
+    ).toInt();
+  }
+
+  /// interpolate Measurements
+  List<Measurement> interpolate(InterpolFunc interpolFunc){
+    final int dateFrom = measures.first.dateInMs - extrapolationRange;
+    final int dateTo = measures.last.dateInMs + extrapolationRange;
+    return <Measurement>[
+      for (int date=dateFrom; date < dateTo; date += extrapolationStepWidth)
+        interpolFunc.function(date, measures) as Measurement
+    ];
+  }
+
+  /// extrapolationRange in milliseconds
+  late int extrapolationRange;
+  /// step widths in milliseconds
+  late int extrapolationStepWidth;
+  /// daily-averaged and sorted Measurements
+  late List<Measurement> measures;
+}
---
+++ app/lib/core/measurementDatabase.dart
@@ -0,0 +1,133 @@
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'package:trale/core/interpolation.dart';
+import 'package:trale/core/measurement.dart';
+import 'package:trale/main.dart';
+
+
+/// check if two integers corresponds to same day
+bool sameDay(DateTime? d1, DateTime? d2) {
+  if (d1 == null || d2 == null)
+    return false;
+  return d1.year == d2.year && d1.month == d2.month && d1.day == d2.day;
+}
+
+/// check if day is in list
+bool dayInMeasurements(DateTime d1, List<Measurement> measurements) =>
+    <bool>[
+      for (final Measurement m in measurements)
+        sameDay(d1, m.date)
+    ].reduce((bool value, bool element) => value || element);
+
+
+/// class providing an API to handle measurements stored in hive
+class MeasurementDatabase {
+  /// constructor
+  MeasurementDatabase();
+
+  static final String _boxName = measurementBoxName;
+
+  /// get unsorted measurements
+  Box<Measurement> get box => Hive.box<Measurement>(_boxName);
+
+  /// get sorted measurements
+  List<Measurement> get measurements => box.values.toList()..sort(
+      (Measurement a, Measurement b) => a.compareTo(b)
+  );
+
+  /// get sorted measurements, key tuples
+  List<SortedMeasurement> get sortedMeasurements => <SortedMeasurement>[
+      for (final dynamic key in box.keys)
+        SortedMeasurement(key: key, measurement: box.get(key)!)
+    ]..sort(
+      (SortedMeasurement a, SortedMeasurement b) => a.compareTo(b)
+    );
+
+  /// get sorted list of daily-averaged measurements
+  List<Measurement> get dailyAveragedMeasurements {
+    final List<Measurement> ms = measurements;
+    if (ms.isEmpty)
+      return measurements;
+    final List<Measurement> dailyAverage = <Measurement>[];
+    final List<double> dailyWeightAverage = <double>[];
+
+    DateTime lastDate = DateTime(
+      ms[0].date.year, ms[0].date.month, ms[0].date.day, _offsetInH,
+    );
+    for (int idx=0; idx < ms.length; idx++){
+      final DateTime date = ms[idx].date;
+      if (!sameDay(date, lastDate)) {
+        dailyAverage.add(
+          Measurement(
+            weight: dailyWeightAverage.reduce(
+                (double value, double element) => value + element
+              ) / dailyWeightAverage.length,
+            date: lastDate,
+          )
+        );
+
+        // get new date and clear list
+        dailyWeightAverage.clear();
+        lastDate = DateTime(date.year, date.month, date.day, _offsetInH);
+      }
+      dailyWeightAverage.add(ms[idx].weight);
+    }
+    // add last element
+    dailyAverage.add(Measurement(
+      weight: dailyWeightAverage.reduce(
+              (double value, double element) => value + element
+      ) / dailyWeightAverage.length,
+      date: lastDate,
+    ));
+    return dailyAverage;
+  }
+
+  /// get linearly interpolated and sorted list of daily-averaged measurements
+  List<Measurement> get dailyAveragedInterpolatedMeasurements {
+    final List<Measurement> ms = dailyAveragedMeasurements;
+    final List<Measurement> dailyMeasurements = <Measurement>[];
+
+    final int intervalInDays =
+      ms.last.date.difference(ms.first.date).inDays + 2 * _offsetInDays;
+    final DateTime dateStart = ms.first.date.subtract(
+      const Duration(days: _offsetInDays)
+    );
+    for (int days = 0; days < intervalInDays; days += 1) {
+      DateTime date = dateStart.add(Duration(days: days));
+      date = DateTime(date.year, date.month, date.day, _offsetInH);
+      bool isDayInMeasurements = false;
+      for (final Measurement m in ms) {
+        if (sameDay(date, m.date)) {
+          dailyMeasurements.add(
+            Measurement(
+              weight: m.weight,
+              date: m.date,
+              isMeasured: true,
+            )
+          );
+          isDayInMeasurements = true;
+          break;
+        }
+      }
+      if (!isDayInMeasurements) {
+        dailyMeasurements.add(
+          Measurement(
+            weight: (InterpolFunc.linear.function(
+              date.millisecondsSinceEpoch, ms,
+            ) as Measurement).weight,
+            date: date,
+            isMeasured: days < _offsetInDays ||
+              days > intervalInDays - _offsetInDays,
+          )
+        );
+      }
+    }
+    return dailyMeasurements;
+  }
+
+  /// offset of day in hours
+  static const int _offsetInH = 12;
+  /// offset of day in interpolation
+  static const int _offsetInDays = 7;
+}
