3d523992362085dab284d8bc9431da8f5eb416c3
braniii <daniel.nagel@protonmail.com>
2021-10-25 09:16:52 +0200

    Extend string class to upper case and all upper case.

---
+++ app/lib/core/stringExtension.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// Inspired by Günter Zöchbauer
+/// https://stackoverflow.com/a/29629114
+extension CapExtension on String {
+  /// capitalize first char
+  String get inCaps => isNotEmpty
+      ? '${this[0].toUpperCase()}${substring(1)}'
+      : '';
+  /// capitalize first char in each word
+  String get allInCaps => replaceAll(
+      RegExp(' +'), ' '
+  ).split(' ').map((String str) => str.inCaps).join(' ');
+}
