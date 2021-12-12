657d904370b25da97db66ec86b5cca57f19ace6d
braniii <daniel.nagel@protonmail.com>
2021-12-12 15:19:24 +0100

    Extension for adding gaps to List<Widget>

---
+++ app/lib/core/gap.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// extend widget list
+extension GapListExtension on List<Widget> {
+  /// Add padding between each widget
+  List<Widget> addGap({
+    required double padding, required Axis direction,
+  }) {
+    final Widget gap = SizedBox(
+      height: direction == Axis.vertical ? padding : 0,
+      width: direction == Axis.horizontal ? padding : 0,
+    );
+    for (int i=length; i >= 0; i--)
+      insert(i, gap);
+    return this;
+  }
+}
