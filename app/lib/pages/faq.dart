b4e9d6fecc1066aed31a83c8a368b661ce016974
braniii <daniel.nagel@protonmail.com>
2022-05-10 21:54:27 +0200

    Improve draft of FAQ

--- app/lib/pages/faq.dart
+++ app/lib/pages/faq.dart
@@ -1,13 +1,14 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 import 'package:trale/core/gap.dart';
 import 'package:trale/core/icons.dart';
 import 'package:trale/core/stringExtension.dart';
 import 'package:trale/core/theme.dart';
+import 'package:trale/core/traleNotifier.dart';
 import 'package:trale/widget/customSliverAppBar.dart';
 
 
@@ -20,38 +21,83 @@ Future<void> _launchURL(String url) async {
   }
 }
 
+
+/// ListTile for changing Amoled settings
+class OnBoardingListTile extends StatelessWidget {
+  /// constructor
+  const OnBoardingListTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      dense: true,
+      title: AutoSizeText(
+        AppLocalizations.of(context)!.factoryReset,
+        style: Theme.of(context).textTheme.bodyText1,
+        maxLines: 1,
+      ),
+      contentPadding: EdgeInsets.symmetric(
+        horizontal: 2 * TraleTheme.of(context)!.padding,
+      ),
+      subtitle: AutoSizeText(
+        AppLocalizations.of(context)!.factoryResetSubtitle,
+        style: Theme.of(context).textTheme.overline,
+      ),
+      trailing: IconButton(
+        icon: const Icon(CustomIcons.events),
+        onPressed: () {
+          Provider.of<TraleNotifier>(
+              context, listen: false
+          ).showOnBoarding = true;
+          // leave settings
+          Navigator.of(context).pop();
+        },
+      ),
+    );
+  }
+}
+
+
 /// class for listing 3rd party licences
 class FAQEntry {
   /// constructor
   FAQEntry({
     required this.question,
     required this.answer,
-    this.answerWidget=const SizedBox.shrink(),
+    this.answerWidget,
   });
 
   /// get list representation of tpl
-  Widget toWidget(BuildContext context) => Container(
-    padding: EdgeInsets.all(
-      TraleTheme.of(context)!.padding,
-    ),
-    child: Column(
-      children: <Widget>[
-        Text(
+  Widget toWidget(BuildContext context) => Column(
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.fromLTRB(
+          TraleTheme.of(context)!.padding,
+          TraleTheme.of(context)!.padding,
+          TraleTheme.of(context)!.padding,
+          0,
+        ),
+        child: Text(
           'Q: $question',
           style: Theme.of(context).textTheme.bodyText1!.copyWith(
             fontWeight: FontWeight.bold,
           ),
           textAlign: TextAlign.justify,
         ),
-        SizedBox(height: TraleTheme.of(context)!.padding),
-        Text(
+      ),
+      Padding(
+        padding: EdgeInsets.all(
+          TraleTheme.of(context)!.padding,
+        ),
+        child: Text(
           'A: $answer',
           style: Theme.of(context).textTheme.bodyText1,
           textAlign: TextAlign.justify,
         ),
-        answerWidget,
-      ]
-    ),
+      ),
+      if (answerWidget != null)
+        answerWidget!
+    ]
   );
 
   /// name of package
@@ -59,26 +105,8 @@ class FAQEntry {
   /// name of url
   final String answer;
   /// license
-  final Widget answerWidget;
+  final Widget? answerWidget;
 }
-
-/// list of questions and answers
-final List<FAQEntry> faqentries = <FAQEntry>[
-  FAQEntry(
-    question: 'The feature X is missing. When will it be implemented?',
-    answer: 'Probably never. This app was created in our free time. '
-        'If you miss a feature feel free to open an issue or implement '
-        'on your own.'
-    ,
-  ),
-  FAQEntry(
-    question: 'The feature X is missing. When will it be implemented?',
-    answer: 'Probably never. This app was created in our free time. '
-      'If you miss a feature feel free to open an issue or implement '
-      'on your own.'
-    ,
-  ),
-];
 
 /// about screen widget class
 class FAQ extends StatefulWidget {
@@ -89,37 +117,34 @@ class FAQ extends StatefulWidget {
 class _FAQ extends State<FAQ> {
   @override
   Widget build(BuildContext context) {
+    /// list of questions and answers
+    final List<FAQEntry> faqentries = <FAQEntry>[
+      FAQEntry(
+        question: AppLocalizations.of(context)!.faq_q1,
+        answer: AppLocalizations.of(context)!.faq_a1,
+      ),
+      FAQEntry(
+        question: 'The feature X is missing. When will it be implemented?',
+        answer: 'Probably never. This app was created in our free time. '
+            'If you miss a feature feel free to open an issue or implement '
+            'on your own.'
+        ,
+        answerWidget: const OnBoardingListTile(),
+      ),
+    ];
+
     Widget faqList() {
       return ListView(
         padding: EdgeInsets.all(TraleTheme.of(context)!.padding),
         children: <Widget>[
           Padding(
             padding: EdgeInsets.all(TraleTheme.of(context)!.padding),
-            child: const Text(
-              'This app completely respects your privacy. This means that we '
-              'do not earn anything with it and we do not get error logs. If '
-              'you are facing problems, please open an issue at GitLab.\n\n'
-              'The purpose of this app is to provide a simple log of the '
-              ' weight together with a short-term extrapolation. Nothing more,'
-              ' but nothing less. Therefore, we do not plan to add fancy '
-              'features.\n\n'
-              'We are only two devs with little sparse time. If you like the '
-              'work consider contributing or donating. \u{1F642}',
+            child: Text(
+              AppLocalizations.of(context)!.faqtext,
               textAlign: TextAlign.justify,
             ),
           ),
           Divider(height: 2 * TraleTheme.of(context)!.padding),
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: TraleTheme.of(context)!.padding,
-            ),
-            child: AutoSizeText(
-              AppLocalizations.of(context)!.faq.allInCaps,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headline5,
-              maxLines: 1,
-            ),
-          ),
           ...<Widget>[
             for (FAQEntry faq in faqentries)
               faq.toWidget(context),
