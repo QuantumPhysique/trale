06eabe838d86e8ba9e48c6cdcf8dde2e41eb79c7
Patrick Binder <p.binder@dkfz-heidelberg.de>
2022-05-16 21:19:31 +0200

    Improve padding for dummyChart

--- app/lib/pages/overview.dart
+++ app/lib/pages/overview.dart
@@ -52,12 +52,15 @@ class _OverviewScreen extends State<OverviewScreen> {
     );
 
     final SizedBox dummyChart = SizedBox(
-      height: MediaQuery.of(context).size.height / 3,
+      height: MediaQuery.of(context).size.height / 2,
       width: MediaQuery.of(context).size.width,
       child: Card(
         shape: TraleTheme.of(context)!.borderShape,
-        margin: EdgeInsets.symmetric(
-          horizontal: TraleTheme.of(context)!.padding,
+        margin: EdgeInsets.fromLTRB(
+          TraleTheme.of(context)!.padding,
+          0,
+          TraleTheme.of(context)!.padding,
+          TraleTheme.of(context)!.padding,
         ),
         child: Center(
           child: RichText(
