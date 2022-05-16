7239bdb57f14c4cd0639712ed79f626fa1549ebf
Patrick Binder <p.binder@dkfz-heidelberg.de>
2022-05-16 22:10:22 +0200

    Improve WeightList

--- app/lib/pages/weightList.dart
+++ app/lib/pages/weightList.dart
@@ -22,120 +22,140 @@ class _OverviewScreen extends State<WeightList> {
     final List<SortedMeasurement> measurements = database.sortedMeasurements;
     const String groupTag = 'groupTag';
 
-    final ListView listOfView = ListView.builder(
-        clipBehavior: Clip.antiAlias,
-        itemCount: measurements.length,
-        itemBuilder: (BuildContext context, int index) {
-          final SortedMeasurement currentMeasurement
-          = measurements[index];
-          Widget deleteAction() {
-            return SlidableAction(
-                label: AppLocalizations.of(context)!.delete,
-                backgroundColor: Theme.of(context).colorScheme.tertiaryContainer,
-                foregroundColor: Theme.of(context).colorScheme.onTertiaryContainer,
-                icon: CustomIcons.delete,
-                onPressed: (BuildContext context) {
-                  database.deleteMeasurement(currentMeasurement);
-                  setState(() {});
-                  final SnackBar snackBar = SnackBar(
-                    content: const Text('Measurement was deleted'),
-                    behavior: SnackBarBehavior.floating,
-                    width: MediaQuery.of(context).size.width / 3 * 2,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(
-                                TraleTheme.of(context)!.borderRadius
-                            )
-                        )
-                    ),
-                    action: SnackBarAction(
-                      label: 'Undo',
-                      onPressed: () {
-                        database.insertMeasurement(
-                            currentMeasurement.measurement
-                        );
-                        setState(() {});
-                      },
-                    ),
+    Widget deleteAction(SortedMeasurement currentMeasurement) {
+      return SlidableAction(
+          label: AppLocalizations.of(context)!.delete,
+          backgroundColor: Theme.of(context).colorScheme.tertiaryContainer,
+          foregroundColor: Theme.of(context).colorScheme.onTertiaryContainer,
+          icon: CustomIcons.delete,
+          onPressed: (BuildContext context) {
+            database.deleteMeasurement(currentMeasurement);
+            setState(() {});
+            final SnackBar snackBar = SnackBar(
+              content: const Text('Measurement was deleted'),
+              behavior: SnackBarBehavior.floating,
+              width: MediaQuery.of(context).size.width / 3 * 2,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(
+                          TraleTheme.of(context)!.borderRadius
+                      )
+                  )
+              ),
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () {
+                  database.insertMeasurement(
+                      currentMeasurement.measurement
                   );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
-            );
-          }
-          Widget editAction() {
-            return SlidableAction(
-              label: AppLocalizations.of(context)!.edit,
-              backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-              foregroundColor: Theme.of(context).colorScheme.onSecondaryContainer,
-              icon: CustomIcons.edit,
-              onPressed: (BuildContext context) async {
-                final bool changed = await showAddWeightDialog(
-                  context: context,
-                  weight: currentMeasurement.measurement.weight,
-                  date: currentMeasurement.measurement.date,
-                );
-                if (changed) {
-                  database.deleteMeasurement(currentMeasurement);
                   setState(() {});
-                }
-              },
+                },
+              ),
             );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
           }
-          return SlidableAutoCloseBehavior(
-            child: Slidable(
-              groupTag: groupTag,
-              startActionPane: ActionPane(
-                motion: const DrawerMotion(),
-                extentRatio: 0.75,
-                children: <Widget>[
-                  deleteAction(),
-                  editAction(),
-                ],
-              ),
-              endActionPane: ActionPane(
-                motion: const DrawerMotion(),
-                extentRatio: 0.75,
-                children: <Widget>[
-                  editAction(),
-                  deleteAction()
-                ],
-              ),
-              closeOnScroll: true,
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.center,
-                    //color: Theme.of(context).colorScheme.background,
-                    width: MediaQuery.of(context).size.width
-                        - 2 * TraleTheme.of(context)!.padding,
-                    height: 50.0,
-                    child: Text(
-                      currentMeasurement.measurement.measureToString(
-                        context, ws: 12,
-                      ),
-                      style: Theme.of(context).textTheme
-                          .bodyText1?.apply(fontFamily: 'Courier'),
-                    ),
+      );
+    }
+
+    Widget editAction(SortedMeasurement currentMeasurement) {
+      return SlidableAction(
+        label: AppLocalizations.of(context)!.edit,
+        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+        foregroundColor: Theme.of(context).colorScheme.onSecondaryContainer,
+        icon: CustomIcons.edit,
+        onPressed: (BuildContext context) async {
+          final bool changed = await showAddWeightDialog(
+            context: context,
+            weight: currentMeasurement.measurement.weight,
+            date: currentMeasurement.measurement.date,
+          );
+          if (changed) {
+            database.deleteMeasurement(currentMeasurement);
+            setState(() {});
+          }
+        },
+      );
+    }
+
+    List<Slidable> listOfMeasurements = measurements.map(
+      (SortedMeasurement currentMeasurement) {
+        return Slidable(
+          groupTag: groupTag,
+          startActionPane: ActionPane(
+            motion: const DrawerMotion(),
+            extentRatio: 0.75,
+            children: <Widget>[
+              deleteAction(currentMeasurement),
+              editAction(currentMeasurement),
+            ],
+          ),
+          endActionPane: ActionPane(
+            motion: const DrawerMotion(),
+            extentRatio: 0.75,
+            children: <Widget>[
+              editAction(currentMeasurement),
+              deleteAction(currentMeasurement)
+            ],
+          ),
+          closeOnScroll: true,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                //color: Theme.of(context).colorScheme.background,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width
+                    - 2 * TraleTheme.of(context)!.padding,
+                height: 50.0,
+                child: Text(
+                  currentMeasurement.measurement.measureToString(
+                    context, ws: 12,
                   ),
-                ],
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodyText1
+                      ?.apply(fontFamily: 'Courier'),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    ).toList();
+
+    
+    return SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.of(context).size.width
+            - 2 * TraleTheme.of(context)!.padding,
+        height: measurements.length * 50.0
+            + 2 * TraleTheme.of(context)!.padding,
+        padding: EdgeInsets.symmetric(
+            vertical: TraleTheme.of(context)!.padding
+        ),
+        child: Card(
+          shape: TraleTheme.of(context)!.borderShape,
+          color: Theme.of(context).colorScheme.surface,
+          margin: EdgeInsets.symmetric(
+            horizontal: TraleTheme.of(context)!.padding,
+          ),
+          child: SlidableAutoCloseBehavior(
+            child: ClipRRect(
+              borderRadius:
+                TraleTheme.of(context)!.borderShape.borderRadius.resolve(
+                  Directionality.of(context)
+                ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: listOfMeasurements,
               ),
             ),
-          );
-        }
-    );
-
-    return Container(
-      width: MediaQuery.of(context).size.width
-          - 2 * TraleTheme.of(context)!.padding,
-      padding: EdgeInsets.symmetric(vertical: TraleTheme.of(context)!.padding),
-      child: Card(
-        shape: TraleTheme.of(context)!.borderShape,
-        color: Theme.of(context).colorScheme.surface,
-        margin: EdgeInsets.symmetric(
-          horizontal: TraleTheme.of(context)!.padding,
+          )
         ),
-        child: listOfView
       ),
     );
   }
