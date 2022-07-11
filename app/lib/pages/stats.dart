38122e1c94762d73d255e2c190bba9b0d2634b63
braniii <daniel.nagel@protonmail.com>
2022-07-11 23:57:53 +0200

    Include draft of stats.

--- app/lib/pages/stats.dart
+++ app/lib/pages/stats.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:trale/core/icons.dart';
+import 'package:trale/core/gap.dart';
 import 'package:trale/core/measurement.dart';
 import 'package:trale/core/measurementDatabase.dart';
 import 'package:trale/core/stringExtension.dart';
@@ -23,6 +24,48 @@ class _StatsScreen extends State<StatsScreen> {
       horizontal: TraleTheme.of(context)!.padding,
     );
 
+    database.longestMeasurementStrike;
+    Card getCard(String label, Measurement m) => Card(
+      shape: TraleTheme.of(context)!.borderShape,
+      margin: EdgeInsets.symmetric(
+        vertical: TraleTheme.of(context)!.padding,
+      ),
+      color: Theme.of(context).colorScheme.secondaryContainer,
+      child: Padding(
+        padding: EdgeInsets.all(TraleTheme.of(context)!.padding),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            AutoSizeText(
+              m.dateToString(context),
+              style: Theme.of(context).textTheme.caption,
+            ),
+            AutoSizeText(
+              '$label: ${m.weightToString(context)}',
+              style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                color: Theme.of(context).colorScheme.onSecondaryContainer,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    final Widget minmax_widget = FractionallySizedBox(
+      widthFactor: 1,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(child: getCard('min', database.min!)),
+          Expanded(child: getCard('max', database.max!)),
+        ].addGap(
+          padding: TraleTheme.of(context)!.padding,
+          direction: Axis.horizontal,
+        ),
+      ),
+    );
+
     return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -36,6 +79,7 @@ class _StatsScreen extends State<StatsScreen> {
               maxLines: 1,
             ),
           ),
+          minmax_widget,
           Padding(
             padding: padding,
             child: AutoSizeText(
