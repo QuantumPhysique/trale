28e33fe6d530f224b67bcacd8c09d6b5ea701bdf
braniii <daniel.nagel@protonmail.com>
2023-11-14 16:11:06 +0100

    Enforce using lockfile and patch rive_common.

---
+++ app/patch_pubdev.sh
@@ -0,0 +1,13 @@
+# This file is intendet to patch packages from pub.dev
+#
+# Path rive_common to remove build-id
+RIVE_COMMON_VERSION="$(grep -A7 -P '^..rive_common' pubspec.lock | sed -E -n -e 's/^.*version:\ "([0-9.]+)"/\1/p')"
+
+# get path to pub_cache directory
+if [[ -v PUB_CACHE ]]; then
+    PUB_PATH="$PUB_CACHE"
+else
+    PUB_PATH="$HOME/.pub_cache"
+fi
+
+patch "${PUB_PATH}/hosted/pub.dev/rive_common-${RIVE_COMMON_VERSION}/premake5_rive_plugin.lua" "rive_common.patch"
