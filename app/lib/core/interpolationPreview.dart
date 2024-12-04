20ee5ee4dabdf86c40738a49795558da5103f5dc
braniii <daniel.nagel@protonmail.com>
2024-12-04 22:30:55 +0100

    Refactor MeasurementDatabase and MeasurementInterpolation to make it reusable for preview graph.

---
+++ app/lib/core/interpolationPreview.dart
@@ -0,0 +1,38 @@
+import 'package:trale/core/measurement.dart';
+import 'package:trale/core/measurementDatabase.dart';
+import 'package:trale/core/measurementInterpolation.dart';
+
+
+List<Measurement> _ms = <Measurement>[
+  Measurement(
+    weight: 78.9,
+    date: DateTime.now().subtract(Duration(days: 10)),
+  ),
+  Measurement(
+    weight: 78.3,
+    date: DateTime.now().subtract(Duration(days: 2)),
+  ),
+  Measurement(
+      weight: 78.4,
+      date: DateTime.now().subtract(Duration(days: 1)),
+  ),
+  Measurement(weight: 78.1, date: DateTime.now()),
+];
+
+
+
+class PreviewDatabase extends MeasurementDatabaseBaseclass {
+  PreviewDatabase();
+
+  @override
+  List<Measurement> get measurements => _ms;
+}
+
+
+class PreviewInterpolation extends MeasurementInterpolationBaseclass {
+  PreviewInterpolation();
+
+  /// get measurements
+  @override
+  PreviewDatabase get db => PreviewDatabase();
+}
--- app/lib/core/measurementDatabase.dart
+++ app/lib/core/measurementDatabase.dart
@@ -29,8 +29,80 @@ bool dayInMeasurements(DateTime date, List<Measurement> measurements) =>
     ].reduce((bool value, bool element) => value || element);
 
 
+/// Base class for measurement database
+class MeasurementDatabaseBaseclass {
+  MeasurementDatabaseBaseclass();
+
+  /// broadcast stream to track change of db
+  final StreamController<List<Measurement>> _streamController =
+  StreamController<List<Measurement>>.broadcast();
+
+  /// get broadcast stream to track change of db
+  StreamController<List<Measurement>> get streamController => _streamController;
+
+  List<Measurement>? _measurements;
+  /// get sorted measurements
+  List<Measurement> get measurements => _measurements == null
+    ? <Measurement>[]
+    : _measurements!..sort(
+        (Measurement a, Measurement b) => b.compareTo(a)
+    );
+
+
+  /// fire stream
+  void fireStream() {
+    streamController.add(measurements);
+  }
+
+  /// get mean measurements
+  List<Measurement> averageMeasurements(List<Measurement> ms) {
+    if (ms.isEmpty) {
+      return ms;
+    }
+
+    final double meanWeight = ms.fold(
+        0.0, (double sum, Measurement m) => sum + m.weight
+    ) / ms.length;
+    return <Measurement>[
+      for (final Measurement m in ms)
+        m.apply(weight: meanWeight)
+    ];
+  }
+
+  /// get number of measurements
+  int get nMeasurements => measurements.length;
+
+  /// date of latest measurement
+  DateTime get lastDate => measurements.first.date;
+  /// date of first measurement
+  DateTime get firstDate => measurements.last.date;
+
+  /// get largest measurement
+  Measurement? get max {
+    if (measurements.isEmpty) {
+      return null;
+    }
+    return measurements.reduce(
+            (Measurement current, Measurement next) =>
+        current.weight > next.weight ? current : next
+    );
+  }
+
+  /// get lowest measurement
+  Measurement? get min {
+    if (measurements.isEmpty) {
+      return null;
+    }
+    return measurements.reduce(
+            (Measurement current, Measurement next) =>
+        current.weight < next.weight ? current : next
+    );
+  }
+}
+
+
 /// class providing an API to handle measurements stored in hive
-class MeasurementDatabase {
+class MeasurementDatabase extends MeasurementDatabaseBaseclass {
   /// singleton constructor
   factory MeasurementDatabase() => _instance;
 
@@ -44,13 +116,6 @@ class MeasurementDatabase {
 
   /// get box
   Box<Measurement> get box => Hive.box<Measurement>(_boxName);
-
-  /// broadcast stream to track change of db
-  final StreamController<List<Measurement>> _streamController =
-    StreamController<List<Measurement>>.broadcast();
-
-  /// get broadcast stream to track change of db
-  StreamController<List<Measurement>> get streamController => _streamController;
 
   /// check if measurement exists
   bool containsMeasurement(Measurement m) {
@@ -84,7 +149,6 @@ class MeasurementDatabase {
     if (count > 0) {
       reinit();
     }
-
     return count;
   }
 
@@ -125,32 +189,14 @@ class MeasurementDatabase {
     sortedMeasurements;
   }
 
+  @override
   List<Measurement>? _measurements;
   /// get sorted measurements
+  @override
   List<Measurement> get measurements => _measurements ??=
     box.values.toList()..sort(
-      (Measurement a, Measurement b) => a.compareTo(b)
+      (Measurement a, Measurement b) => b.compareTo(a)
     );
-
-  /// fire stream
-  void fireStream() {
-    streamController.add(measurements);
-  }
-
-  /// get mean measurements
-  List<Measurement> averageMeasurements(List<Measurement> ms) {
-    if (ms.isEmpty) {
-      return ms;
-    }
-
-    final double meanWeight = ms.fold(
-        0.0, (double sum, Measurement m) => sum + m.weight
-    ) / ms.length;
-    return <Measurement>[
-      for (final Measurement m in ms)
-        m.apply(weight: meanWeight)
-    ];
-  }
 
   List<SortedMeasurement>? _sortedMeasurements;
   /// get sorted measurements, key tuples
@@ -162,44 +208,12 @@ class MeasurementDatabase {
       (SortedMeasurement a, SortedMeasurement b) => b.compareTo(a)
     );
 
-  /// get number of measurements
-  int get nMeasurements => measurements.length;
-
   /// date of latest measurement
+  @override
   DateTime get lastDate => sortedMeasurements.first.measurement.date;
   /// date of first measurement
+  @override
   DateTime get firstDate => sortedMeasurements.last.measurement.date;
-
-  /// bmi at last measurement
-  // double? get bmi {
-  //   final double? height = Preferences().userHeight;
-  //   if (height == null || gaussianInterpolatedMeasurements.isEmpty) {
-  //     return null;
-  //   }
-  //   return gaussianInterpolatedMeasurements.last.weight / (height * height);
-  // }
-
-  /// get largest measurement
-  Measurement? get max {
-    if (measurements.isEmpty) {
-      return null;
-    }
-    return measurements.reduce(
-      (Measurement current, Measurement next) =>
-        current.weight > next.weight ? current : next
-    );
-  }
-
-  /// get lowest measurement
-  Measurement? get min {
-    if (measurements.isEmpty) {
-      return null;
-    }
-    return measurements.reduce(
-      (Measurement current, Measurement next) =>
-        current.weight < next.weight ? current : next
-    );
-  }
 
   /// return string for export
   String get exportString {
