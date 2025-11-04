40a4eb6460e13c85219e8f89e070241a7cde5a72
Patrick Binder <47981658+gwosd@users.noreply.github.com>
2025-11-04 22:46:04 +0100

    Improve FAB animation (HERO animation is still missing)

--- app/lib/widget/floatingActionButton.dart
+++ app/lib/widget/floatingActionButton.dart
@@ -30,18 +30,12 @@ class _FABState extends State<FAB> {
     return AnimatedContainer(
         alignment: Alignment.center,
         height: widget.show ? buttonHeight : 0,
-        width: widget.show ? buttonHeight : 0,
-        margin: EdgeInsets.all(
-          widget.show ? 0 : 0.5 * buttonHeight,
-        ),
+        width: buttonHeight,
         duration: TraleTheme.of(context)!.transitionDuration.normal,
-        child: FittedBox(
-          fit: BoxFit.contain,
-          child: M3EFloatingActionButton.medium(
-            elevation: 0,
-            onPressed: widget.onPressed,
-            tooltip: AppLocalizations.of(context)!.addWeight,
-          ),
+        child: M3EFloatingActionButton.medium(
+          elevation: 0,
+          onPressed: widget.onPressed,
+          tooltip: AppLocalizations.of(context)!.addWeight,
         )
     );
   }
