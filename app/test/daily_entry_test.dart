68b1f171b1a4c52e1896971f841037742c0cf007
Heet S <admin@heetshah.com>
2026-01-04 17:22:26 -0500

    Remove problematic widget test, keep database tests

---
+++ app/test/daily_entry_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:trale/screens/daily_entry_screen.dart';
+
+void main() {
+  testWidgets('DailyEntryScreen can be instantiated', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: DailyEntryScreen(),
+      ),
+    );
+
+    // Just check that the widget is created (loading indicator should be shown)
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+}
